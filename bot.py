89077b2cf5dcecdac168502b290e413117a474ec
misschampa <jyotikumari9694276043@gmail.com>
2025-01-16 21:30:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-API_ID = "2359907814"
+API_ID = "23900056"
 API_HASH = "db7e21e638bc2359907814f4ed8b48a8"
 BOT_TOKEN = "7350085099:AAHQ6uYn3uFPpf0o4p_BrDrf9MM4Fz41iSU"
 
